5a33e43224d1a5e5d378c23337fd8a52a67749a7
juandllo <desmon2009@gmail.com>
2024-04-23 21:25:22 -0500

    Se elimina log
    Se elimina log y se organiza estructura del payload

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -16,15 +16,16 @@ export class AccountService {
   ): Promise<{ access_token: string }> {
     const user = await this.usersService.findByEmail(email);
 
-    console.log(`Se he encontrado al usuario ${JSON.stringify(user)}`);
-
     if (!user) throw new NotFoundException(`El usuario ${email} no existe.`);
 
     const validPassword = await compare(password, user.password);
 
     if (!validPassword) throw new HttpException('INVALID_PASSWORD', 403);
 
-    const payload = { sub: user.email, ...user };
+    const payload = {
+      sub: JSON.parse(JSON.stringify(user)),
+      role: [...user.roles],
+    };
 
     return {
       access_token: await this.jwtService.signAsync(payload),
